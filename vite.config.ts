a40b18c92b88e7cc6c40204b1767f84a06c53055
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-05-08 08:18:37 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { build, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-    {
-      find: './runtimeConfig',
-      replacement: './runtimeConfig.browser',
-    },
-  ]
-}
-})
-
-
+      {
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
+      },
+    ],
+  },
+  // build: {
+  //   outDir: "build",
+  // }
+});
