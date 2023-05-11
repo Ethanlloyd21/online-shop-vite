272a448253bd4876af78550cfa8633ca0cffda3a
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-05-11 14:53:17 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-// import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import Home from "./screens/home/Home";
+import {
+  defaultDarkModeOverride,
+  ThemeProvider,
+  ColorMode,
+} from "@aws-amplify/ui-react";
+
 import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  const [colorMode, setColorMode] = useState<ColorMode>("dark");
+  const theme = {
+    name: "my-theme",
+    overrides: [defaultDarkModeOverride],
+  };
+
   return (
     <>
-      <div>
-        <h1>Vite + React</h1>
-        <p>This is the code to start your app: <code>npm run dev</code></p>
-      </div>
+      <ThemeProvider theme={theme} colorMode={colorMode}>
+        <Home />
+      </ThemeProvider>
+
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Cards.tsx
@@ -0,0 +1,49 @@
+import {
+    Card,
+    Flex,
+    Badge,
+    StepperField,
+    Button,
+    Image,
+    Text,
+  } from "@aws-amplify/ui-react";
+  import viteLogo from "/vite.svg";
+  
+  const Cards = () => {
+    return (
+      <>
+        <Card variation="elevated">
+          <Flex alignItems="flex-start">
+            <Image src={viteLogo} alt="Amplify" width="8rem" />
+            <Flex direction="column" gap="xs">
+              <Flex>
+                <Badge variation="success">New</Badge>
+              </Flex>
+              <div style={{textAlign: "left"}}>
+                <Text fontSize="large" fontWeight="semibold">
+                  Product title
+                </Text>
+                <Text color="font.tertiary">Product description</Text>
+                <Text fontSize="large" color="secondary">
+                  $199.99
+                </Text>
+              </div>
+              <Flex>
+                <StepperField
+                  label="Quantity"
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={1}
+                  labelHidden
+                />
+                <Button variation="primary">Add to cart</Button>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Card>
+      </>
+    );
+  };
+  
+  export default Cards;
