89f1c031a5fa2c43e8f29427e0dc6dfc253a5509
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-05-12 12:16:18 -0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from './redux/store';
-import App from "./App.tsx";
+import store from "./redux/store";
+import App from "./App";
 import "./index.css";
 import "@aws-amplify/ui-react/styles.css";
 
